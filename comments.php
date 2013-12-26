a37a2f04be2c8ca0935b7bd8d67a2b8838b694f5
Kazue Igarashi <igarashi@digitalcube.jp>
2013-12-26 20:13:24 +0900

    comment form

--- comments.php
+++ comments.php
@@ -23,15 +23,27 @@ if ( post_password_required() ) {
 <div id="comments" class="comments-area">
 	<?php do_action( 'tidy_before_comments' ); ?>
 
+	<?php if ( have_comments() ) : ?>
+	<h2 class="comments-title">
+		<?php
+			printf( _nx( 'Comment', 'Comments', get_comments_number(), 'comments title', 'tidy' ),
+				number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+		?>
+	</h2>
+	<?php endif; ?>
+
+	<?php /*
+		// If comments are closed and there are comments, let's leave a little note, shall we?
+		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+	?>
+		<p class="no-comments"><?php _e( 'Comments are closed.', 'tidy' ); ?></p>
+	<?php endif; */?>
+
+	<?php comment_form(); ?>
+
 	<?php // You can start editing here -- including this comment! ?>
 
 	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'tidy' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
-			?>
-		</h2>
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
 		<nav id="comment-nav-above" class="comment-navigation" role="navigation">
@@ -62,15 +74,6 @@ if ( post_password_required() ) {
 		<?php endif; // check for comment navigation ?>
 
 	<?php endif; // have_comments() ?>
-
-	<?php
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-	?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'tidy' ); ?></p>
-	<?php endif; ?>
-
-	<?php comment_form(); ?>
 
 	<?php do_action( 'tidy_after_comments' ); ?>
 </div><!-- #comments -->
