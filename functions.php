664b459c7aacf60dba540257de81c511b116b811
Kazue Igarashi <igarashi@digitalcube.jp>
2014-01-09 16:45:02 +0900

    webフォント（アイコンフォント）修正

--- functions.php
+++ functions.php
@@ -139,13 +139,13 @@ function tidy_scripts() {
 
 	wp_enqueue_style(
 		'genericons',
-		get_template_directory_uri() . '/css/genericons.css',
+		get_template_directory_uri() . '/genericons/genericons.min.css',
 		array(),
 		'3.0.2'
 	);
 	wp_enqueue_style(
 		'iconmoon-tidy',
-		get_template_directory_uri() . '/css/iconmoon-tidy.css',
+		get_template_directory_uri() . '/iconmoon-tidy/style.min.css',
 		array(),
 		$tidy_version
 	);
